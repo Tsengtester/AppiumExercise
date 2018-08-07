f72d380ce11fa167cf38cbac7ce5e1150c90b43a
yaya.tseng <yaya.tseng@isuncloud.com>
2018-08-07 18:33:37 +0800

    Add Cases

--- features/Android/step_definitions/step.rb
+++ features/Android/step_definitions/step.rb
@@ -1,17 +1,22 @@
+When /^I retrieve wallet$/ do
+    $driver.find_element(id:$element_table['retrieveWalletBtn']).click
+end
+
 Given /^I agree terms$/ do
     $driver.find_element(id: $element_table['agreeTermsBtn']).click
 end
 
-When /^I retrieve wallet$/ do
-    $driver.find_element(id:$element_table['retrieveWalletBtn']).click
-    sleep(3)
-    $driver.find_element(id:$element_table['keystoreBtn']).click
+When /^Retrieve By keyStore$/ do
+    $driver.find_element(id:$element_table['ImportKeystoreBtn']).click
     sleep(3)
     #$driver.find_element(id:$element_table['keystoreTextFiled']).send_keys($keystore)
     $driver.find_element(id:$element_table['keystoreTextFiled']).send_keys("{\"address\":\"a3b77ff951cd9e1af4f51470b67fd1e0fc1c2b63\",\"crypto\":{\"cipher\":\"aes-128-ctr\",\"ciphertext\":\"6eb1cb3ecb62de3d74743aa1a85b4440cb51e0288b1cb827f1894c83a2dcd3be\",\"cipherparams\":{\"iv\":\"33c18a431d7b47b2394b2e6a9d0f484e\"},\"kdf\":\"scrypt\",\"kdfparams\":{\"dklen\":32,\"n\":4096,\"p\":6,\"r\":8,\"salt\":\"e23d0b3d4d46eb626162d040662dfe9604d6ad550e1bf63262ae666aa94bb062\"},\"mac\":\"e4db9b68d391805abf2b0d67ccf03b47406c9c464259799748f27ced40a12ed1\"},\"id\":\"d31c84a1-c985-4dd7-bfca-94acd7bea7ce\",\"version\":3}")
     sleep(3)
     $driver.find_element(id:$element_table['confirmBtn']).click
     sleep(3)
+end
+
+And /^Check keyStore Password$/ do
     is_element_displayed("id", $element_table['confirmPwDialog'])
     sleep(3)
     $driver.find_element(class: $element_table['inputPwTextFiled']).send_keys("test0003")
@@ -33,8 +38,41 @@ Then /^I should see Set Name Text$/ do
 end
   
 Given /^I choose to setting the wallet name$/ do
-    $driver.find_element(id: $element_table['SetNameConfirmBtn']).click
+    $driver.find_element(id: $element_table['SetNameSettingBtn']).click
     
+end
+
+When /^user change the wallet name and submit$/ do
+    $driver.find_element(id: $element_table['WalletNameEdit']).clear
+    $driver.find_element(id: $element_table['WalletNameEdit']).send_keys("QAWallet")
+    $driver.find_element(id: $element_table['WalletNameConfirm']).click
+end
+
+Given /^I choose to add Token Currency$/ do
+    $driver.find_element(id: $element_table['AddCurrency']).click
+end
+
+When /^User Add TWD Currency$/ do
+    $driver.find_element(:xpath, $element_table['TWXSwitch']).click
+    $driver.find_element(:xpath, $element_table['Back']).click
+end
+
+Given /^I choose to retrieve more wallet$/ do
+    $driver.find_element(id: $element_table['WalletIcon']).click
+    $driver.find_element(id: $element_table['ImportWalletIN']).click
+end
+
+When /^Retrieve By Mnemonic$/ do
+    $driver.find_element(id: $element_table['ImportMnemonicBtn']).click
+    sleep(3)
+    $driver.find_element(id: $element_table['Mnemonic']).send_keys("camp spike help need alcohol silk divert ready chat february nuclear trigger")
+    sleep(3)
+    $driver.find_element(:xpath,$element_table['SettingNewPassword']).send_keys("Test123456Test")
+    sleep(3)
+    $driver.find_element(:xpath,$element_table['RepeatNewPassword'] ).send_keys("Test123456Test")
+    sleep(3)
+    $driver.find_element(id: $element_table['ImportConfirmBtn']).click
+    sleep(3) 
 end
 
 Then(/^the element having (.+) "([^\"]*)" should\s*((?:not)?)\s+have text as "(.*?)"$/) do |type, access_name, present, value |
@@ -43,7 +81,7 @@ Then(/^the element having (.+) "([^\"]*)" should\s*((?:not)?)\s+have text as "(.
     access_name = $element_table[access_name]
     validate_locator type
     check_element_text(type, value, access_name, present.empty?)
-  end
+end
 
 
 
--- features/welcome.feature
+++ features/welcome.feature
@@ -2,8 +2,15 @@ Feature: landing page
   
   @firstcase
   Scenario: If user disagree terms, user can't create wallet.
+    When I retrieve wallet
+    Then the element having id "MessageBar" should have text as "You need to read & accept the terms of service"
+   
+
+  Scenario: If user agree terms, user can create wallet.
     Given I agree terms
     When I retrieve wallet
+    And Retrieve By keyStore
+    And Check keyStore Password
     Then I should see Wallet Ready message
 
   
@@ -12,10 +19,30 @@ Feature: landing page
     Then I should see Set Name Text
 
   
-  @last
-  Scenario: if user select to set the wallet name,user can see the setting page.
+  Scenario: if user select to set the wallet name,user can see rename.
     Given I choose to setting the wallet name 
-    Then the element having id "title" should have text as "Rename Wallet"
-    Then the element having id "WalletNameEdit" should have text as "Unnamed Wallet"
+    And the element having id "Functiontitle" should have text as "Rename Wallet"
+    When user change the wallet name and submit
+    Then the element having id "IndexTitle" should have text as "Assets"
+    And the element having id "WalletName" should have text as "QAWallet"
+
+
+  Scenario: User Add Token Currency
+    Given I choose to add Token Currency
+    And the element having id "AddcurrencyTitle" should have text as "Add Token Currency"
+    When User Add TWD Currency
+    Then the element having xpath "TWX" should have text as "TWX"
+
+
+  @last
+  Scenario: Ｕser Can retrieve the other wallet 
+    Given I choose to retrieve more wallet  
+    When Retrieve By Mnemonic
+    Then I should see Wallet Ready message
+
+
+
+  
+
     
 
